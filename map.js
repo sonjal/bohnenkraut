fc8a404e5c64ae23a5da92871f5f3b5dc2f2ed33
Sonja <sonja.list@hotmail.com>
2021-06-27 20:23:50 +0200

    maxZoom added + map resized

--- map.js
+++ map.js
@@ -6,9 +6,9 @@ var map = L.map('garten', {
 var wmsLayer = L.tileLayer.wms('http://vogis.cnv.at/mapserver/mapserv?map=i_luftbilder_r_wms.map&service=wms&', {
 	layers: 'ef2012_12cm',
 	format: 'image/png',
-	//crs: 3857,
+	maxZoom: 25,
 	crs: L.CRS.EPSG3857,
 	attribution: '&copy; <a href="https://vogis.cnv.at">Orthofoto Land Vorarlberg</a> contributors'
 }).addTo(map);
 
-map.setView([47.1877, 9.7069], 20);
+map.setView([47.18356, 9.70087], 20);
